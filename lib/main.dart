0877dc31ed84d39f728849ec784570a7a2f2a578
PushkaraSharma <pushkarasharma11@gmail.com>
2020-04-26 08:27:17 +0530

    "multiplayer button ,user to users list"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:zerokata/startup_screens/FirstView.dart';
+import 'package:zerokata/offline_mode/offline_play.dart';
+import 'package:zerokata/ai_mode/Game.dart';
+//import 'package:flutter_tic_tac_toe/user_list/user_list.dart';
 
 
 void main() => runApp(new MyApp());
@@ -15,6 +18,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.grey,
       ),
       home:  FirstView(title:"ZeroKata"),
+//      routes: <String, WidgetBuilder>{
+//        'AI Mode': (BuildContext context) => Game(title:"AI mode"),
+//        'Offline': (BuildContext context) => Offline(title:"Offline"),
+//        'multiplayerGame': (BuildContext context) =>
+//            Game(title: 'Online'),
+//        'userList': (BuildContext context) => UserList(title: 'All users')
+//      },
     );
   }
 }
